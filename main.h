c3afebcf1b4aa37a49d84fff059fe865a732e0ad
Ezekiel <jezekiel234@gmail.com>
2023-03-28 04:11:11 -0700

    basic main.h done

--- main.h
+++ main.h
@@ -9,7 +9,37 @@
 #define UNUSED(x) (void)(x)
 #define BUFFSIZE 1024
 
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct format - struct operation
+ *
+ * @format: the format
+ * @func: The function associated with the format
+ */
+struct format
+{
+	char format;
+	int (*func)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct format fmt_t - assigning struct format to fmt_t
+ *
+ * @format: the format
+ * @func: function associated with format
+ */
+typedef struct format fmt_t;
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-void _puts(char *str);
+int handle_print(const char *format, int *i, va_list ap, char buffer[],
+		int flags, int width, int precision, int size);
+
+#endif /* endif MAIN_H */
